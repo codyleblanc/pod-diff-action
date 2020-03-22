619ce972452f5139e48ae8e78927a758178219fd
Cody LeBlanc <cody@coderawr.com>
2020-03-22 18:48:17 -0400

    Attempt to make Pull Request

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,34 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import {GitHub, context} from '@actions/github'
 import {LockfileParser} from '@snyk/cocoapods-lockfile-parser'
 import table from 'markdown-table'
+import * as process from 'process'
 
 async function run(): Promise<void> {
   try {
+    const updateCmd = core.getInput('update_cmd')
+    const workingDir = core.getInput('working_dir')
+    const token = core.getInput('token')
+    const commitEmail = core.getInput('commit_email')
+    const commitUsername = core.getInput('commit_username')
+
+    // by default this is just the root directory
+    process.chdir(workingDir)
+
+    // take a snapshot of the current lockfile before updating it
     const currentLockfile: LockfileParser = await LockfileParser.readFile(
       './Podfile.lock'
     )
 
-    const podUpdate = await exec.exec('bundle exec pod update')
+    // run the update command
+    const podUpdate = await exec.exec(updateCmd)
 
     if (podUpdate !== 0) {
       throw new Error('Pod Update Failed')
     }
 
+    // take a new snapshot to compare the before and after
     const newLockfile: LockfileParser = await LockfileParser.readFile(
       './Podfile.lock'
     )
@@ -33,8 +47,56 @@ async function run(): Promise<void> {
       ['Name', 'Old Version', 'New Version'],
       ...updateRows
     ])
-    core.info('Packages were updated')
+
+    core.info('Packages were updated:')
     core.info(markdownTable)
+
+    const github = new GitHub(token)
+
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const branch = `update_pods/${timestamp}`
+
+    if (
+      0 !== (await exec.exec(`git config --global user.name ${commitUsername}`))
+    ) {
+      throw Error("Couldn't set Username")
+    }
+
+    if (
+      0 !== (await exec.exec(`git config --global user.email ${commitEmail}`))
+    ) {
+      throw Error("Couldn't set Email")
+    }
+    if (0 !== (await exec.exec(`git checkout -b ${branch}`))) {
+      throw Error("Couldn't create a new Branch")
+    }
+    if (0 !== (await exec.exec('git add Podfile.lock'))) {
+      throw Error("Couldn't add Podfile")
+    }
+    if (0 !== (await exec.exec('git commit -m "Update Pods"'))) {
+      throw Error("Couldn't create commit")
+    }
+    if (
+      0 !==
+      (await exec.exec(
+        `git push -f https://x-access-token:${token}@github.com/${context.repo.repo}.git HEAD:refs/heads/${branch}`
+      ))
+    ) {
+      throw Error("Couldn't couldn't push")
+    }
+
+    const createRequest = await github.pulls.create({
+      ...context.repo,
+      title: 'Update Pods',
+      base: context.ref,
+      head: branch,
+      body: markdownTable,
+      maintainer_can_modify: true
+    })
+
+    if (createRequest.status !== 200) {
+      throw Error('Issue creating Pull Request')
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
